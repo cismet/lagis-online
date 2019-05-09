b4344fd5153976890a4ff99d2cbaeb3720e61365
helllth <th@cismet.de>
2019-05-09 10:54:06 +0200

    changed redirection of / and some footer text

--- src/containers/DefaultLayout/DefaultFooter.js
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -2,24 +2,27 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-  children: PropTypes.node,
+	children: PropTypes.node
 };
 
 const defaultProps = {};
 
 class DefaultFooter extends Component {
-  render() {
+	render() {
+		// eslint-disable-next-line
+		const { children, ...attributes } = this.props;
 
-    // eslint-disable-next-line
-    const { children, ...attributes } = this.props;
-
-    return (
-      <React.Fragment>
-        <span><a href="https://coreui.io">CoreUI</a> &copy; 2018 creativeLabs.</span>
-        <span className="ml-auto">Powered by <a href="https://coreui.io/react">CoreUI for React</a></span>
-      </React.Fragment>
-    );
-  }
+		return (
+			<React.Fragment>
+				<span>
+					<a href='https://cismet.de'>LagIS-online</a> &copy; 2019.
+				</span>
+				<span className='ml-auto'>
+					Powered by <a href='https://cismet.de'>cids</a>
+				</span>
+			</React.Fragment>
+		);
+	}
 }
 
 DefaultFooter.propTypes = propTypes;
