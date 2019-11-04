6b79460370fce8512c755940a9e4d7797a190601
helllth <th@cismet.de>
2019-11-04 16:31:24 +0100

    added storybook

---
+++ src/views/LagisHistorie/Historie.stories.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('History', module).add('First Story', () => <h1>First</h1>);
--- src/views/LagisKassenzeichen/Kassenzeichen.js
+++ src/views/LagisKassenzeichen/Kassenzeichen.js
@@ -1,20 +1,14 @@
-import React, {Component} from 'react';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  Card,
-  CardBody,
-  CardHeader,
-  Col,
-  Row
-} from 'reactstrap';
+import React, { Component } from 'react';
+import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Col, Row } from 'reactstrap';
 
 class Breadcrumbs extends Component {
-  render() {
-    return (
-      <div className="animated fadeIn"></div>
-    );
-  }
+	render() {
+		return (
+			<div className='animated fadeIn'>
+				<h1>Kasssenzeichen</h1>
+			</div>
+		);
+	}
 }
 
 export default Breadcrumbs;
