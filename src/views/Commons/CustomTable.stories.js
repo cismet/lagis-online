573b9335f6210a05f3caad9904cd72ebf3ab9393
therter <thorsten.herter@cismet.de>
2019-11-13 11:01:23 +0100

    wip #1 correct the number of data sets in the customTable stories

--- src/views/Commons/CustomTable.stories.js
+++ src/views/Commons/CustomTable.stories.js
@@ -19,7 +19,65 @@ storiesOf('CustomTable', module).add('CustomTable With 0 entries', () => {
                 data={data}
                 />
 
-}).add('CustomTable With five entries', () => {
+}).add('CustomTable With 2 entries', () => {
+    const cols = [{
+        Header: 'Dienststelle',
+        accessor: 'dienststelle'
+    }, {
+        Header: 'Fläche in m²',
+        accessor: 'flaeche'
+    }];
+
+    const data = [{
+        dienststelle: 'Wasser, Boden, Abfall1',
+        flaeche: 65536
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall2',
+        flaeche: 1000
+    }]
+
+    return <CustomTable 
+                columns={cols}
+                data={data}
+                />
+
+}).add('CustomTable With 5 entries', () => {
+    const cols = [{
+        Header: 'Dienststelle',
+        accessor: 'dienststelle'
+    }, {
+        Header: 'Fläche in m²',
+        accessor: 'flaeche'
+    }];
+
+    const data = [{
+        dienststelle: 'Wasser, Boden, Abfall1',
+        flaeche: 65536
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall2',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall3',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall4',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall5',
+        flaeche: 1000
+    }]
+
+    return <CustomTable 
+                columns={cols}
+                data={data}
+                />
+
+}).add('CustomTable With 25 entries', () => {
     const cols = [{
         Header: 'Dienststelle',
         accessor: 'dienststelle'
@@ -66,6 +124,64 @@ storiesOf('CustomTable', module).add('CustomTable With 0 entries', () => {
     },
     {
         dienststelle: 'Wasser, Boden, Abfall11',
+        flaeche: 1000
+    },{
+        dienststelle: 'Wasser, Boden, Abfall1',
+        flaeche: 65536
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall2',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall3',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall4',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall5',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall6',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall8',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall9',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall10',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall11',
+        flaeche: 1000
+    },{
+        dienststelle: 'Wasser, Boden, Abfall1',
+        flaeche: 65536
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall2',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall3',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall4',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall5',
         flaeche: 1000
     }]
 
