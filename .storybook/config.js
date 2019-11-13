d3201ac9f476f23c2c1b3af54daaee84f7eb22d2
therter <thorsten.herter@cismet.de>
2019-11-13 10:37:31 +0100

    #1 adjust the storybook css configuration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,6 @@
 import { configure } from '@storybook/react';
+import '../src/index.css';
+import '../src/App.scss';
 const req = require.context('../src/views', true, /\.stories\.js$/);
 
 function loadStories() {
