a6c3f278e55e8024d236e9655064125ac7a242bd
therter <thorsten.herter@cismet.de>
2019-11-13 10:33:43 +0100

    #1 add the first version of the CustomTable

---
+++ src/views/Commons/CustomTable.stories.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import CustomTable from './CustomTable';
+import LagisMap from './LagisMap';
+
+storiesOf('CustomTable', module).add('CustomTable With 0 entries', () => {
+    const cols = [{
+        Header: 'Dienststelle',
+        accessor: 'dienststelle'
+    }, {
+        Header: 'Fläche in m²',
+        accessor: 'flaeche'
+    }];
+
+    const data = []
+
+    return <CustomTable 
+                columns={cols}
+                data={data}
+                />
+
+}).add('CustomTable With five entries', () => {
+    const cols = [{
+        Header: 'Dienststelle',
+        accessor: 'dienststelle'
+    }, {
+        Header: 'Fläche in m²',
+        accessor: 'flaeche'
+    }];
+
+    const data = [{
+        dienststelle: 'Wasser, Boden, Abfall1',
+        flaeche: 65536
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall2',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall3',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall4',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall5',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall6',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall8',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall9',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall10',
+        flaeche: 1000
+    },
+    {
+        dienststelle: 'Wasser, Boden, Abfall11',
+        flaeche: 1000
+    }]
+
+    return <CustomTable 
+                columns={cols}
+                data={data}
+                />
+
+});
+
+
