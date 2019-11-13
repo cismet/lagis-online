0656d79ef7c723adb6582eefe5452751595bc3e6
therter <thorsten.herter@cismet.de>
2019-11-13 11:24:43 +0100

    wip #1 add the LagisMap story

---
+++ src/views/Commons/LagisMap.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import LagisMap from './LagisMap';
+
+
+storiesOf('Lagis-Map', module).add('Lagis-Map', () => <LagisMap />);
