dde376bad40242a75cf9d8eec45c2b990c4428e0
therter <thorsten.herter@cismet.de>
2019-11-14 10:42:03 +0100

    wip #1 workaround for the firefox checkbox styling issues

--- src/views/LagisMipa/MipaDetail.js
+++ src/views/LagisMipa/MipaDetail.js
@@ -1,103 +1,143 @@
 import React from 'react';
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  Col,
-  Row,
-  FormGroup,
-  Label,
-  Input
-} from 'reactstrap';
-
+import { Card, CardBody, CardHeader, Col, Row, FormGroup, Label, Input } from 'reactstrap';
 
 const merkmalExists = (merkmal, mipa) => {
-    if (mipa != null) {
-        var merkmale = mipa.ar_mipa_merkmale;
+	if (mipa != null) {
+		var merkmale = mipa.ar_mipa_merkmale;
 
-        if (merkmale != null) {
-            for (var index = 0; index < merkmale.length; ++index) {
-                if (merkmale[index].bezeichnung === merkmal) {
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
-}
+		if (merkmale != null) {
+			for (var index = 0; index < merkmale.length; ++index) {
+				if (merkmale[index].bezeichnung === merkmal) {
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+};
 
-
-const MipaDetail = ({mipa: data}) => {
-  return (
-      <div>
-      <Row>
-          <Col lg="4" xs="4">
-              <Card>
-                  <CardHeader>
-                      <i className="fa fa-align-justify"></i>Merkmale
-                  </CardHeader>
-                  <CardBody>
-                    <FormGroup row>
-                      <Col>
-                        <FormGroup check className="checkbox">
-                          <Input className="form-check-input" type="checkbox" id="ckUnentgeltlich" name="checkbox1" value="unentgeltlich" readOnly checked={merkmalExists('Unentgeltlich', data)}/>
-                          <Label check className="form-check-label" htmlFor="ckUnentgeltlich">Unentgeltlich</Label>
-                        </FormGroup>
-                        <FormGroup check className="checkbox">
-                          <Input className="form-check-input" type="checkbox" id="ckKeineAkte" name="checkbox2" value="keineAkte" readOnly checked={merkmalExists('keine Akte', data)} />
-                          <Label check className="form-check-label" htmlFor="ckKeineAkte">keine Akte</Label>
-                        </FormGroup>
-                        <FormGroup check className="checkbox">
-                          <Input className="form-check-input" type="checkbox" id="ckAltlast" name="checkbox3" value="Altlast" readOnly checked={merkmalExists('Altlast', data)} />
-                          <Label check className="form-check-label" htmlFor="ckAltlast">Altlast</Label>
-                        </FormGroup>
-                        <FormGroup check className="checkbox">
-                          <Input className="form-check-input" type="checkbox" id="ckBiotop" name="checkbox3" value="biotop" readOnly checked={merkmalExists('Biotop', data)} />
-                          <Label check className="form-check-label" htmlFor="ckBiotop">Biotop</Label>
-                        </FormGroup>
-                        <FormGroup check className="checkbox">
-                          <Input className="form-check-input" type="checkbox" id="ckGestaltung" name="checkbox3" value="gestaltung" readOnly checked={merkmalExists('Gestaltung', data)} />
-                          <Label check className="form-check-label" htmlFor="ckGestaltung">Gestaltung</Label>
-                        </FormGroup>
-                      </Col>
-                    </FormGroup>
-                  </CardBody>
-              </Card>
-          </Col>
-          {/*Querverweise werden über eine Suche gefunden und sind nicht im Flurstueck enthalten-*/}
-          <Col lg="4" xs="4">
-              <Card>
-                  <CardHeader>
-                      <i className="fa fa-align-justify"></i>Querverweise
-                  </CardHeader>
-                  <CardBody>
-                      <FormGroup row>
-                        <div style={{height: "105px"}}>
-                            <div>Langerfeld 450 46/0</div>
-                            <div>Langerfeld 449 118/0</div>
-                            <div>Langerfeld 449 126/0</div>
-                        </div>
-                      </FormGroup>
-                  </CardBody>
-              </Card>
-          </Col>
-          <Col lg="4" xs="4">
-              <Card>
-                  <CardHeader>
-                      <i className="fa fa-align-justify"></i>Bemerkung
-                  </CardHeader>
-                  <CardBody>
-                    <FormGroup row>
-                      <Col xs="12" md="9">
-                        <Input type="textarea" style={{height: "105px"}} name="textarea-input" id="textarea-input" readOnly value={(data === null ? ' ' : data.bemerkung)} />
-                      </Col>
-                    </FormGroup>
-                  </CardBody>
-              </Card>
-          </Col>
-      </Row>
-      </div>
-      );
-}
+const MipaDetail = ({ mipa: data }) => {
+	return (
+		<div>
+			<Row>
+				<Col lg="3" xs="3">
+					<Card>
+						<CardHeader>
+							<i className="fa fa-align-justify" />Merkmale
+						</CardHeader>
+						<CardBody>
+							<FormGroup row>
+								<Col sm={{ size: 6, order: 2, offset: 1 }}>
+									<FormGroup check className="checkbox">
+										<Input
+											className="form-check-input"
+											type="checkbox"
+											id="ckUnentgeltlich"
+											name="checkbox1"
+											value="unentgeltlich"
+											readOnly
+											checked={merkmalExists('Unentgeltlich', data)}
+										/>
+										<span style={{ verticalAlign: 'middle', paddingTop: '5px' }}>
+											Unentgeltlich
+										</span>
+										{/* Do not use the label tag, because firefox has some styling issues, when
+                                            using checkboxes with a label
+                                            <Label check className="form-check-label" htmlFor="ckUnentgeltlich">
+											Unentgeltlich
+										</Label> */}
+									</FormGroup>
+									<FormGroup check className="checkbox">
+										<Input
+											className="form-check-input"
+											type="checkbox"
+											id="ckKeineAkte"
+											name="checkbox2"
+											value="keineAkte"
+											readOnly
+											checked={merkmalExists('keine Akte', data)}
+										/>
+										<span style={{ verticalAlign: 'middle', paddingTop: '5px' }}>keine Akte</span>
+									</FormGroup>
+									<FormGroup check className="checkbox">
+										<Input
+											className="form-check-input"
+											type="checkbox"
+											id="ckAltlast"
+											name="checkbox3"
+											value="Altlast"
+											readOnly
+											checked={merkmalExists('Altlast', data)}
+										/>
+										<span style={{ verticalAlign: 'middle', paddingTop: '5px' }}>Altlast</span>
+									</FormGroup>
+									<FormGroup check className="checkbox">
+										<Input
+											className="form-check-input"
+											type="checkbox"
+											id="ckBiotop"
+											name="checkbox3"
+											value="biotop"
+											readOnly
+											checked={merkmalExists('Biotop', data)}
+										/>
+										<span style={{ verticalAlign: 'middle', paddingTop: '5px' }}>Biotop</span>
+									</FormGroup>
+									<FormGroup check className="checkbox">
+										<Input
+											className="form-check-input"
+											type="checkbox"
+											id="ckGestaltung"
+											name="checkbox3"
+											value="gestaltung"
+											readOnly
+											checked={merkmalExists('Gestaltung', data)}
+										/>
+										<span style={{ verticalAlign: 'middle', paddingTop: '5px' }}>Gestaltung</span>
+									</FormGroup>
+								</Col>
+							</FormGroup>
+						</CardBody>
+					</Card>
+				</Col>
+				{/*Querverweise werden über eine Suche gefunden und sind nicht im Flurstueck enthalten-*/}
+				<Col lg="4" xs="4">
+					<Card>
+						<CardHeader>
+							<i className="fa fa-align-justify" />Querverweise
+						</CardHeader>
+						<CardBody>
+							<FormGroup row>
+								<div style={{ height: '105px' }}>
+									<div>Langerfeld 450 46/0</div>
+									<div>Langerfeld 449 118/0</div>
+									<div>Langerfeld 449 126/0</div>
+								</div>
+							</FormGroup>
+						</CardBody>
+					</Card>
+				</Col>
+				<Col lg="5" xs="5">
+					<Card>
+						<CardHeader>
+							<i className="fa fa-align-justify" />Bemerkung
+						</CardHeader>
+						<CardBody>
+							<FormGroup row>
+								<Input
+									type="textarea"
+									style={{ height: '105px', marginLeft: '15px', marginRight: '15px' }}
+									name="textarea-input"
+									id="textarea-input"
+									value={data === null ? ' ' : data.bemerkung}
+								/>
+							</FormGroup>
+						</CardBody>
+					</Card>
+				</Col>
+			</Row>
+		</div>
+	);
+};
 
 export default MipaDetail;
--- src/views/LagisVerwaltungsbereiche/Verwaltungsbereiche.js
+++ src/views/LagisVerwaltungsbereiche/Verwaltungsbereiche.js
@@ -5,29 +5,37 @@ import RollenTabelle from './RollenTabelle.js';
 import StrassenfrontTabelle from './StrassenfrontTabelle';
 import { FormGroup, Label, Input, Card, CardBody, CardHeader, Col, Row } from 'reactstrap';
 import LagisMap from '../Commons/LagisMap';
+import GetMapSize from '../Commons/SizeHelper.js';
 import jsonData from './../../assets/json/fs_22726.json';
+import './test.css';
 //import jsonData from './../../assets/json/fs_23316.json';
 
 const Verwaltungsbereiche = () => {
 	return (
 		<div className="animated fadeIn" style={{ margin: '10px' }}>
 			<Row>
-				<Col xs="20" lg="7">
+				<Col>
 					<Row>
-						<Col lg="12">
-							<DienststellenTabelle flurstueck={jsonData} />
+						<Col lg="8" style={{ height: GetMapSize() + 88 + 'px' }}>
+							<DienststellenTabelle style={{ height: '100%' }} flurstueck={jsonData} />
+						</Col>
+						<Col lg="4" style={{ height: '100%' }}>
+							<Card>
+								<CardHeader>Karte</CardHeader>
+								<CardBody>
+									<LagisMap />
+								</CardBody>
+							</Card>
 						</Col>
 					</Row>
 					<Row>
-						<Col xs="12" lg="6">
+						<Col lg="4">
 							<RollenTabelle flurstueck={jsonData} />
 						</Col>
-						<Col xs="12" lg="6">
+						<Col lg="4">
 							<StrassenfrontTabelle flurstueck={jsonData} />
 						</Col>
-					</Row>
-					<Row>
-						<Col xs="12" lg="6">
+						<Col lg="4">
 							<Card>
 								<CardHeader>
 									<i className="fa fa-align-justify" /> Bemerkungen/Sperre
@@ -45,24 +53,14 @@ const Verwaltungsbereiche = () => {
 									</FormGroup>
 
 									<FormGroup style={{ float: 'left' }}>
-										<Label check>
-											Sperre&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-											<Input type="checkbox" />
-											{}
-										</Label>
+										{/*Do not use the label tag for checkboxes, because firefox has some styling issues with checkboxes*/}
+										<span style={{ verticalAlign: 'middle', paddingTop: '5px' }}>Sperre</span>
+										<Input style={{ marginLeft: '5px' }} id="sperre" type="checkbox" />
 									</FormGroup>
 								</CardBody>
 							</Card>
 						</Col>
 					</Row>
-				</Col>
-				<Col xs="13" lg="5">
-					<Card>
-						<CardHeader>Karte</CardHeader>
-						<CardBody>
-							<LagisMap />
-						</CardBody>
-					</Card>
 				</Col>
 			</Row>
 		</div>
