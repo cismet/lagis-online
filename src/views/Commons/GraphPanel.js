1d445b1adea692373840ea7508f22d83d14e50de
therter <thorsten.herter@cismet.de>
2019-11-15 10:45:55 +0100

    wip #1 increase the width of the graphs to 100%

--- src/views/Commons/GraphPanel.js
+++ src/views/Commons/GraphPanel.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Graphviz } from 'graphviz-react';
 
-const GraphPanel = ({ graphCode: code }) => {
-	return <Graphviz dot={code} />;
+const GraphPanel = ({ graphCode: code, options: o, style: customStyle }) => {
+	useEffect(() => {
+		// var div = document.getElementsByTagName('svg')[0];
+		var div = document.getElementById('graphvizDiv');
+		if (div != null) {
+			var svg = div.firstChild.firstChild;
+			svg.style['width'] = '100%';
+		}
+	});
+
+	return (
+		<div id="graphvizDiv">
+			<Graphviz style={customStyle} dot={code} options={o} />
+		</div>
+	);
 };
 
 export default GraphPanel;
