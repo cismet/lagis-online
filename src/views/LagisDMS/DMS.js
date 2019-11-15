6ff100d676c314244a675d52976e6ff5571e59b1
therter <thorsten.herter@cismet.de>
2019-11-15 10:12:59 +0100

    wip #1 extract dms data from flurstueck

--- src/views/LagisDMS/DMS.js
+++ src/views/LagisDMS/DMS.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Col, Row } from 'reactstrap';
+import React from 'react';
 import FilePanel from '../Commons/FilePanel.js';
 import jsonData from './../../assets/json/fs_17778.json';
 
+const extractDmsData = (flurstueck) => {
+	var dms = [];
+	var dmsUrls = flurstueck.n_dms_urls;
+
+	if (dmsUrls != null) {
+		for (var index = 0; index < dmsUrls.length; ++index) {
+			var object = {};
+
+			if (dmsUrls[index].fk_url != null) {
+				object.server = dmsUrls[index].fk_url.server;
+				object.path = dmsUrls[index].fk_url.path;
+			}
+
+			object.name = dmsUrls[index].name;
+
+			dms.push(object);
+		}
+	}
+
+	return dms;
+};
+
 const DMS = () => {
-	return <FilePanel dmsUrls={jsonData} />;
+	return <FilePanel dmsUrls={extractDmsData(jsonData)} />;
 };
 
 export default DMS;
